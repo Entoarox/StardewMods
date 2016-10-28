94d5bbbf8d0ddf0d8d074707f74a3c805c31005f
Entoarox <kvdk@kvdk.net>
2016-10-28 21:04:12 +0200

    comit

---
+++ SeasonalBuildings/Mod.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework.Graphics;
+
+using StardewValley;
+using StardewValley.Buildings;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Entoarox.SeasonalBuildings
+{
+    internal class ContentPackManifest
+    {
+        public string Name;
+        public string Author;
+        public Version Version;
+        public string Description;
+    }
+    internal class SeasonalBuildingsMod : StardewModdingAPI.Mod
+    {
+        public override void Entry(params object[] objects)
+        {
+            StardewModdingAPI.Events.GameEvents.LoadContent += GameEvents_LoadContent;
+            FilePath = PathOnDisk;
+            string mode = "xna";
+            if(Type.GetType("Microsoft.Xna.Framework.Input.Joystick")!=null)
+                mode = "mono";
+            StardewModdingAPI.Log.SyncColour("[SeasonalBuildings] Version "+GetType().Assembly.GetName().Version+"/"+mode+" by Entoarox, do not redistribute", ConsoleColor.Cyan);
+        }
+        private static string FilePath;
+        private static Dictionary<string, Dictionary<string, Texture2D>> SeasonTextures = new Dictionary<string, Dictionary<string, Texture2D>>();
+        private static string[] seasons = new string[] { "spring", "summer", "fall", "winter" };
+        internal static void GameEvents_LoadContent(object s, EventArgs e)
+        {
+            string path = Path.Combine(FilePath, "ContentPack.zip");
+            ZipArchive zip;
+            if (File.Exists(path))
+                zip = new ZipArchive(new FileStream(path, FileMode.Open), ZipArchiveMode.Update);
+            else
+            {
+                Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Entoarox.SeasonalBuildings.ContentPack.zip");
+                if (stream == null)
+                {
+                    StardewModdingAPI.Log.SyncColour("[SeasonalBuildings] Could not resolve the ContentPack.zip file, did you forget to download one?", ConsoleColor.Red);
+                    return;
+                }
+                zip = new ZipArchive(stream,ZipArchiveMode.Update);
+            }
+            ZipArchiveEntry manifestData = zip.GetEntry("manifest.json");
+            ContentPackManifest manifest = JsonConvert.DeserializeObject<ContentPackManifest>(new StreamReader(manifestData.Open()).ReadToEnd(), new VersionConverter());
+            StardewModdingAPI.Log.SyncColour("[SeasonalBuildings] Using the `" + manifest.Name + "` content pack, version " + manifest.Version + " by " + manifest.Author, ConsoleColor.Cyan);
+            foreach (string file in Directory.EnumerateFiles(Path.Combine(Game1.content.RootDirectory, "Buildings")))
+            {
+                Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
+                string name = Path.GetFileNameWithoutExtension(file);
+                foreach (string season in seasons)
+                {
+                    ZipArchiveEntry zipFile = zip.GetEntry(season + '/' + name + ".png");
+                    if (zipFile == null)
+                        goto skipFile;
+                    textures.Add(season, Texture2D.FromStream(Game1.graphics.GraphicsDevice, zipFile.Open()));
+                }
+                SeasonTextures.Add(name, textures);
+            skipFile:
+                continue;
+            }
+            StardewModdingAPI.Events.GameEvents.UpdateTick += GameEvents_UpdateTick;
+        }
+        internal static void GameEvents_UpdateTick(object s, EventArgs e)
+        {
+            if (!Game1.hasLoadedGame || Game1.getFarm() == null)
+                return;
+            TimeEvents_SeasonOfYearChanged(s, e);
+            StardewModdingAPI.Events.TimeEvents.SeasonOfYearChanged += TimeEvents_SeasonOfYearChanged;
+            StardewModdingAPI.Events.LocationEvents.CurrentLocationChanged += LocationEvents_CurrentLocationChanged;
+            StardewModdingAPI.Events.GameEvents.UpdateTick -= GameEvents_UpdateTick;
+        }
+        internal static void TimeEvents_SeasonOfYearChanged(object s, EventArgs e)
+        {
+            if (SeasonTextures.ContainsKey("houses"))
+                Game1.getFarm().houseTextures = SeasonTextures["houses"][Game1.currentSeason];
+            foreach (Building building in Game1.getFarm().buildings)
+                if (SeasonTextures.ContainsKey(building.buildingType))
+                    building.texture = SeasonTextures[building.buildingType][Game1.currentSeason];
+        }
+        internal static void LocationEvents_CurrentLocationChanged(object s, EventArgs e)
+        {
+            if (Game1.currentLocation.name != "Farm")
+                return;
+            StardewModdingAPI.Events.LocationEvents.CurrentLocationChanged -= LocationEvents_CurrentLocationChanged;
+            TimeEvents_SeasonOfYearChanged(s, e);
+        }
+    }
+}
---
+++ packages/Pathoschild.Stardew.ModBuildConfig.1.2.0/README.md
@@ -0,0 +1,81 @@
+﻿**Stardew.ModBuildConfig** is an open-source NuGet package which automates the build configuration
+for crossplatform [Stardew Valley](http://stardewvalley.net/) mods that use SMAPI.
+
+## Usage
+Basically this package lets you write your mod once, and compile it on any computer. It detects
+your current platform (Linux, Mac, or Windows) and game path, and injects the right references
+automatically. You can also target a specific platform to create a mod package compatible with that
+platform.
+
+More specifically, the configuration...
+
+1. detects the operating system and Stardew Valley path;
+2. injects the right references to Stardew Valley, SMAPI, and XNA/MonoGame for your platform;
+3. configures Visual Studio so you can launch the game for debugging (_Windows only_);
+4. and adds a `GamePath` variable which can be used to script mod packaging if desired.
+
+## Installation
+### Creating a new mod
+1. Create an empty library project.
+2. Reference the [`Pathoschild.Stardew.ModBuildConfig` NuGet package](https://www.nuget.org/packages/Pathoschild.Stardew.ModBuildConfig).
+3. [Write your code](http://canimod.com/guides/creating-a-smapi-mod).
+4. Compile on any platform.
+
+### Migrating an existing mod
+1. Remove any references to `Microsoft.Xna.*`, Stardew Valley, `StardewModdingAPI`, and xTile.
+2. Reference the [`Pathoschild.Stardew.ModBuildConfig` NuGet package](https://www.nuget.org/packages/Pathoschild.Stardew.ModBuildConfig).
+3. Compile on any platform.
+
+## Configuration
+### Custom game path
+If you customised where Stardew Valley is installed, you can specify where it is.
+
+1. Get the full path to the directory containing the Stardew Valley executable.
+2. Add this section to your `.csproj` file (anywhere before the added `<Import` line):
+   
+   ```
+   <PropertyGroup>
+     <GamePath>C:\Program Files (x86)\GalaxyClient\Games\Stardew Valley</GamePath>
+   </PropertyGroup>
+   ```
+
+The configuration will check your custom path first, then fall back to the default paths. (That way
+you can still compile it normally on a different computer.)
+
+### Target platform
+By default the build configuration will target your current platform (e.g. Linux, Mac, or Windows).
+If you're compiling it for a different platform (and have the required dependencies installed), you
+can manually override the platform detection.
+
+You can define it...
+
+* in your `.csproj` (anywhere before the added `<Import` line). Valid values are `Linux`, `Mac`, or
+  `Windows`.
+   
+   ```
+   <PropertyGroup>
+     <GamePlatform>Windows</GamePlatform>
+   </PropertyGroup>
+   ```
+
+* _or_ by setting one of these compile constant: `GAME_PLATFORM_LINUX`, `GAME_PLATFORM_MAC`, or
+  `GAME_PLATFORM_WINDOWS`.
+  * <small>In Visual Studio: right-click on the project and choose _Properties_. Click the _Build_
+    tab, and enter the constants into the _Conditional compilation symbols_ field.</small>
+  * <small>In MonoDevelop: right-click on the project and choose _Options_. Click the
+    _Build » Compiler_ tab, and enter the constants into the _Define Symbols_ field.</small>
+
+### Compatibility with mod builders
+The configuration is designed for compatibility with third-party mod compilers. [Silverplum](https://github.com/rumangerst/SilVerPLuM)
+is officially supported, and mod builds can set the following environment variables:
+
+* `GAMEPATH`: overrides the Stardew Valley install path.
+* `GAMEPLATFORM`: overrides the detected platform. Should be only of `Linux`, `Mac`, or `Windows`.
+
+## Versions
+* 1.0: initial release.
+* 1.1: added support for targeting platforms.
+
+## See also
+* [NuGet package](https://www.nuget.org/packages/Pathoschild.Stardew.ModBuildConfig)
+* <s>Discussion thread</s>
