924d0528249a12c18043c627874e3edf8d5ba033
Entoarox <kvdk@kvdk.net>
2016-12-16 18:08:55 +0100

    upload update info.

--- ExtendedMinecart/Mod.cs
+++ ExtendedMinecart/Mod.cs
@@ -7,7 +7,8 @@ using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Menus;
 
-using Farmhand.UI;
+using Entoarox.Framework;
+using Entoarox.Framework.UI;
 
 namespace Entoarox.ExtendedMinecart
 {
@@ -21,6 +22,18 @@ namespace Entoarox.ExtendedMinecart
         public bool WoodsDestinationEnabled = true;
         public bool BeachDestinationEnabled = true;
         public bool WizardDestinationEnabled = true;
+    }
+    internal static class GLExtension
+    {
+        public static void SetTile(this GameLocation self, int x, int y, int index ,string layer, int sheet=0)
+        {
+            EntoFramework.GetLocationHelper().SetStaticTile(self, layer, x, y, index, self.map.TileSheets[sheet].Id);
+        }
+        public static void SetTile(this GameLocation self, int x, int y, int index, string layer, string value, int sheet = 0)
+        {
+            EntoFramework.GetLocationHelper().SetStaticTile(self, layer, x, y, index, self.map.TileSheets[sheet].Id);
+            EntoFramework.GetLocationHelper().SetTileProperty(self, layer, x, y, "Action", value);
+        }
     }
     public class ExtendedMinecart : Mod
     {
@@ -87,155 +100,186 @@ namespace Entoarox.ExtendedMinecart
             // # Farm
             if (Config.FarmDestinationEnabled)
             {
-                GameLocation farm = Game1.getFarm();
-                if (Config.AlternateFarmMinecart)
+                try
                 {
-                    farm.setMapTileIndex(18, 5, 483, "Front", 1);
-                    farm.setMapTileIndex(19, 5, 484, "Front", 1);
-                    farm.setMapTileIndex(19, 5, 217, "Buildings", 1);
-                    farm.setMapTileIndex(20, 5, 485, "Front", 1);
+                    GameLocation farm = Game1.getFarm();
+                    if (Config.AlternateFarmMinecart)
+                    {
+                        farm.SetTile(18, 5, 483, "Front", 1);
+                        farm.SetTile(19, 5, 484, "Front", 1);
+                        farm.SetTile(19, 5, 217, "Buildings", 1);
+                        farm.SetTile(20, 5, 485, "Front", 1);
 
-                    farm.setMapTileIndex(18, 6, 508, "Buildings", 1);
-                    farm.setMapTileIndex(19, 6, 509, "Back", 1);
-                    farm.setMapTileIndex(20, 6, 510, "Buildings", 1);
+                        farm.SetTile(18, 6, 508, "Buildings", 1);
+                        farm.SetTile(19, 6, 509, "Back", 1);
+                        farm.SetTile(20, 6, 510, "Buildings", 1);
 
-                    farm.setMapTileIndex(18, 7, 533, "Buildings", 1);
-                    farm.setMapTileIndex(19, 7, 534, "Back", 1);
-                    farm.setMapTileIndex(20, 7, 535, "Buildings", 1);
+                        farm.SetTile(18, 7, 533, "Buildings", 1);
+                        farm.SetTile(19, 7, 534, "Back", 1);
+                        farm.SetTile(20, 7, 535, "Buildings", 1);
 
-                    farm.setMapTileIndex(19, 6, 933, "Buildings", 1);
-                    farm.setMapTile(19, 7, 958, "Buildings", "MinecartTransport", 1);
+                        farm.SetTile(19, 6, 933, "Buildings", 1);
+                        farm.SetTile(19, 7, 958, "Buildings", "MinecartTransport", 1);
+                    }
+                    else
+                    {
+                        // Clear annoying flower
+                        farm.removeTile(79, 12, "Buildings");
+                        // Cut dark short
+                        farm.SetTile(77, 11, 375, "Back", 1);
+                        farm.SetTile(78, 11, 376, "Back", 1);
+                        farm.SetTile(79, 11, 376, "Back", 1);
+                        // Lay tracks
+                        farm.SetTile(78, 12, 729, "Back", 1);
+                        farm.SetTile(78, 13, 754, "Back", 1);
+                        farm.SetTile(78, 14, 755, "Back", 1);
+                        farm.SetTile(79, 12, 730, "Back", 1);
+                        // Trim grass
+                        farm.SetTile(77, 13, 175, "Back", 1);
+                        farm.SetTile(77, 14, 175, "Back", 1);
+                        farm.SetTile(77, 15, 175, "Back", 1);
+                        farm.SetTile(78, 15, 175, "Back", 1);
+                        farm.SetTile(79, 13, 175, "Back", 1);
+                        farm.SetTile(79, 14, 175, "Back", 1);
+                        farm.SetTile(79, 15, 175, "Back", 1);
+                        // Clean up fence
+                        farm.SetTile(78, 11, 436, "Buildings", 1);
+                        farm.removeTile(78, 14, "Buildings");
+                        // Plop down minecart
+                        farm.SetTile(78, 12, 933, "Buildings", 1);
+                        farm.SetTile(78, 13, 958, "Buildings", "MinecartTransport", 1);
+                        // Keep exit clear
+                        farm.setTileProperty(78, 14, "Back", "NoFurniture", "T");
+                    }
                 }
-                else
+                catch(Exception err)
                 {
-                    // Clear annoying flower
-                    farm.removeTile(79, 12, "Buildings");
-                    // Cut dark short
-                    farm.setMapTileIndex(77, 11, 375, "Back", 1);
-                    farm.setMapTileIndex(78, 11, 376, "Back", 1);
-                    farm.setMapTileIndex(79, 11, 376, "Back", 1);
-                    // Lay tracks
-                    farm.setMapTileIndex(78, 12, 729, "Back", 1);
-                    farm.setMapTileIndex(78, 13, 754, "Back", 1);
-                    farm.setMapTileIndex(78, 14, 755, "Back", 1);
-                    farm.setMapTileIndex(79, 12, 730, "Back", 1);
-                    // Trim grass
-                    farm.setMapTileIndex(77, 13, 175, "Back", 1);
-                    farm.setMapTileIndex(77, 14, 175, "Back", 1);
-                    farm.setMapTileIndex(77, 15, 175, "Back", 1);
-                    farm.setMapTileIndex(78, 15, 175, "Back", 1);
-                    farm.setMapTileIndex(79, 13, 175, "Back", 1);
-                    farm.setMapTileIndex(79, 14, 175, "Back", 1);
-                    farm.setMapTileIndex(79, 15, 175, "Back", 1);
-                    // Clean up fence
-                    farm.setMapTileIndex(78, 11, 436, "Buildings", 1);
-                    farm.removeTile(78, 14, "Buildings");
-                    // Plop down minecart
-                    farm.setMapTileIndex(78, 12, 933, "Buildings", 1);
-                    farm.setMapTile(78, 13, 958, "Buildings", "MinecartTransport", 1);
-                    // Keep exit clear
-                    farm.setTileProperty(78, 14, "Back", "NoFurniture", "T");
+                    Monitor.Log(LogLevel.Error, "Could not patch the Farm due to a unknown error", err);
                 }
             }
             if (Config.DesertDestinationEnabled)
             {
-                // # Desert
-                GameLocation desert = Game1.getLocationFromName("Desert");
-                xTile.Tiles.TileSheet parent = Game1.getLocationFromName("Mountain").map.GetTileSheet("outdoors");
-                desert.map.AddTileSheet(new xTile.Tiles.TileSheet("z_path_objects_custom_sheet", desert.map, parent.ImageSource, parent.SheetSize, parent.TileSize));
-                desert.map.DisposeTileSheets(Game1.mapDisplayDevice);
-                desert.map.LoadTileSheets(Game1.mapDisplayDevice);
-                if (Config.AlternateDesertMinecart)
+                try
                 {
-                    // Reset
-                    desert.removeTile(34, 4, "Buildings");
-                    // Backdrop
-                    desert.setMapTileIndex(33, 1, 221, "Front");
-                    desert.setMapTileIndex(34, 1, 222, "Front");
-                    desert.setMapTileIndex(35, 1, 223, "Front");
+                    // # Desert
+                    GameLocation desert = Game1.getLocationFromName("Desert");
+                    xTile.Tiles.TileSheet parent = Game1.getLocationFromName("Mountain").map.GetTileSheet("outdoors");
+                    desert.map.AddTileSheet(new xTile.Tiles.TileSheet("z_path_objects_custom_sheet", desert.map, parent.ImageSource, parent.SheetSize, parent.TileSize));
+                    desert.map.DisposeTileSheets(Game1.mapDisplayDevice);
+                    desert.map.LoadTileSheets(Game1.mapDisplayDevice);
+                    if (Config.AlternateDesertMinecart)
+                    {
+                        // Backdrop
+                        desert.SetTile(33, 1, 221, "Front");
+                        desert.SetTile(34, 1, 222, "Front");
+                        desert.SetTile(35, 1, 223, "Front");
 
-                    desert.setMapTileIndex(33, 2, 237, "Front");
-                    desert.setMapTileIndex(34, 2, 254, "Buildings");
-                    desert.setMapTileIndex(34, 2, 238, "Front");
-                    desert.setMapTileIndex(35, 2, 239, "Front");
+                        desert.SetTile(33, 2, 237, "Front");
+                        desert.SetTile(34, 2, 254, "Buildings");
+                        desert.SetTile(34, 2, 238, "Front");
+                        desert.SetTile(35, 2, 239, "Front");
 
-                    desert.setMapTileIndex(33, 3, 253, "Buildings");
-                    desert.setMapTileIndex(34, 3, 254, "Buildings");
-                    desert.setMapTileIndex(35, 3, 255, "Buildings");
+                        desert.SetTile(33, 3, 253, "Buildings");
+                        desert.SetTile(34, 3, 254, "Buildings");
+                        desert.SetTile(35, 3, 255, "Buildings");
 
-                    desert.setMapTileIndex(33, 4, 269, "Buildings");
-                    desert.setMapTileIndex(34, 4, 270, "Back");
-                    desert.setMapTileIndex(35, 4, 271, "Buildings");
-                    // Cart
-                    desert.setMapTileIndex(34, 3, 933, "Front", 2);
-                    desert.setMapTile(34, 4, 958, "Buildings", "MinecartTransport", 2);
+                        desert.SetTile(33, 4, 269, "Buildings");
+                        desert.SetTile(34, 4, 270, "Back");
+                        desert.SetTile(35, 4, 271, "Buildings");
+                        // Cart
+                        desert.SetTile(34, 3, 933, "Front", 2);
+                        desert.SetTile(34, 4, 958, "Buildings", "MinecartTransport", 2);
+                    }
+                    else
+                    {
+                        // Backdrop
+                        desert.SetTile(33, 39, 221, "Front");
+                        desert.SetTile(34, 39, 222, "Front");
+                        desert.SetTile(35, 39, 223, "Front");
+
+                        desert.SetTile(33, 40, 237, "Front");
+                        desert.SetTile(34, 40, 254, "Buildings");
+                        desert.SetTile(34, 40, 238, "Front");
+                        desert.SetTile(35, 40, 239, "Front");
+
+                        desert.SetTile(33, 41, 253, "Buildings");
+                        desert.SetTile(34, 41, 254, "Buildings");
+                        desert.SetTile(35, 41, 255, "Buildings");
+
+                        desert.SetTile(33, 42, 269, "Buildings");
+                        desert.SetTile(34, 42, 270, "Back");
+                        desert.SetTile(35, 42, 271, "Buildings");
+                        // Cart
+                        desert.SetTile(34, 41, 933, "Front", 2);
+                        desert.SetTile(34, 42, 958, "Buildings", "MinecartTransport", 2);
+                    }
                 }
-                else
+                catch (Exception err)
                 {
-                    // Reset
-                    desert.removeTile(34, 42, "Buildings");
-                    // Backdrop
-                    desert.setMapTileIndex(33, 39, 221, "Front");
-                    desert.setMapTileIndex(34, 39, 222, "Front");
-                    desert.setMapTileIndex(35, 39, 223, "Front");
-
-                    desert.setMapTileIndex(33, 40, 237, "Front");
-                    desert.setMapTileIndex(34, 40, 254, "Buildings");
-                    desert.setMapTileIndex(34, 40, 238, "Front");
-                    desert.setMapTileIndex(35, 40, 239, "Front");
-
-                    desert.setMapTileIndex(33, 41, 253, "Buildings");
-                    desert.setMapTileIndex(34, 41, 254, "Buildings");
-                    desert.setMapTileIndex(35, 41, 255, "Buildings");
-
-                    desert.setMapTileIndex(33, 42, 269, "Buildings");
-                    desert.setMapTileIndex(34, 42, 270, "Back");
-                    desert.setMapTileIndex(35, 42, 271, "Buildings");
-                    // Cart
-                    desert.setMapTileIndex(34, 41, 933, "Front", 2);
-                    desert.setMapTile(34, 42, 958, "Buildings", "MinecartTransport", 2);
+                    Monitor.Log(LogLevel.Error, "Could not patch the Desert due to a unknown error", err);
                 }
             }
             if (Config.WoodsDestinationEnabled)
             {
-                // # Woods
-                GameLocation woods = Game1.getLocationFromName("Woods");
-                woods.setMapTileIndex(46, 3, 933, "Front", 1);
-                woods.setMapTile(46, 4, 958, "Buildings", "MinecartTransport", 1);
+                try
+                {
+                    // # Woods
+                    GameLocation woods = Game1.getLocationFromName("Woods");
+                    woods.SetTile(46, 3, 933, "Front", 1);
+                    woods.SetTile(46, 4, 958, "Buildings", "MinecartTransport", 1);
+                }
+                catch (Exception err)
+                {
+                    Monitor.Log(LogLevel.Error, "Could not patch the Woods due to a unknown error", err);
+                }
             }
             if(Config.WizardDestinationEnabled)
             {
-                // # Wizard
-                GameLocation forest = Game1.getLocationFromName("Forest");
-                forest.setMapTileIndex(13, 37, 483, "Front");
-                forest.setMapTileIndex(14, 37, 484, "Front");
-                forest.setMapTileIndex(14, 37, 217, "Buildings");
-                forest.setMapTileIndex(15, 37, 485, "Front");
+                try
+                {
+                    // # Wizard
+                    GameLocation forest = Game1.getLocationFromName("Forest");
+                    forest.SetTile(13, 37, 483, "Front");
+                    forest.SetTile(14, 37, 484, "Front");
+                    forest.SetTile(14, 37, 217, "Buildings");
+                    forest.SetTile(15, 37, 485, "Front");
 
-                forest.setMapTileIndex(13, 38, 508, "Buildings");
-                forest.setMapTileIndex(14, 38, 509, "Back");
-                forest.setMapTileIndex(15, 38, 510, "Buildings");
+                    forest.SetTile(13, 38, 508, "Buildings");
+                    forest.SetTile(14, 38, 509, "Back");
+                    forest.SetTile(15, 38, 510, "Buildings");
 
-                forest.setMapTileIndex(13, 39, 533, "Buildings");
-                forest.setMapTileIndex(15, 39, 535, "Buildings");
+                    forest.SetTile(13, 39, 533, "Buildings");
+                    forest.SetTile(15, 39, 535, "Buildings");
 
-                forest.setMapTileIndex(14, 38, 933, "Buildings");
-                forest.setMapTile(14, 39, 958, "Buildings", "MinecartTransport");
+                    forest.SetTile(14, 38, 933, "Buildings");
+                    forest.SetTile(14, 39, 958, "Buildings", "MinecartTransport");
+                }
+                catch (Exception err)
+                {
+                    Monitor.Log(LogLevel.Error, "Could not patch the Forest due to a unknown error", err);
+                }
             }
             if(Config.BeachDestinationEnabled)
             {
-                // # Beach
-                GameLocation beach = Game1.getLocationFromName("Beach");
-                xTile.Tiles.TileSheet parent = Game1.getLocationFromName("Mountain").map.GetTileSheet("outdoors");
-                beach.map.AddTileSheet(new xTile.Tiles.TileSheet("z_path_objects_custom_sheet", beach.map, parent.ImageSource, parent.SheetSize, parent.TileSize));
-                beach.map.DisposeTileSheets(Game1.mapDisplayDevice);
-                beach.map.LoadTileSheets(Game1.mapDisplayDevice);
-                beach.removeTile(67, 2, "Buildings");
-                beach.setMapTileIndex(67, 2, 933, "Buildings", 2);
-                beach.setMapTile(67, 3, 958, "Buildings", "MinecartTransport", 2);
-                beach.removeTile(67, 5, "Buildings");
-                beach.removeTile(67, 4, "Buildings");
-            }
+                try
+                {
+                    // # Beach
+                    GameLocation beach = Game1.getLocationFromName("Beach");
+                    xTile.Tiles.TileSheet parent = Game1.getLocationFromName("Mountain").map.GetTileSheet("outdoors");
+                    beach.map.AddTileSheet(new xTile.Tiles.TileSheet("z_path_objects_custom_sheet", beach.map, parent.ImageSource, parent.SheetSize, parent.TileSize));
+                    beach.map.DisposeTileSheets(Game1.mapDisplayDevice);
+                    beach.map.LoadTileSheets(Game1.mapDisplayDevice);
+                    beach.removeTile(67, 2, "Buildings");
+                    beach.removeTile(67, 5, "Buildings");
+                    beach.removeTile(67, 4, "Buildings");
+                    beach.SetTile(67, 2, 933, "Buildings", 2);
+                    beach.SetTile(67, 3, 958, "Buildings", "MinecartTransport", 2);
+                }
+                catch (Exception err)
+                {
+                    Monitor.Log(LogLevel.Error, "Could not patch the Beach due to a unknown error", err);
+                }
+        }
         }
         private void MenuEvents_MenuChanged(object s, EventArgs e)
         {
