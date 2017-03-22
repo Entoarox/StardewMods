b5be0ea2deb8ae69d8fe4daf1273d929f2cf855c
Entoarox <kvdk@kvdk.net>
2017-03-22 02:48:02 +0100

    trying to fix IL's

--- Framework/Reflection/HookedLocation.cs
+++ Framework/Reflection/HookedLocation.cs
@@ -6,56 +6,55 @@ using StardewValley;
 
 using xTile.Dimensions;
 
-using Microsoft.Xna.Framework.Graphics;
-
 namespace Entoarox.Framework.Reflection
 {
     internal abstract class HookedLocation : GameLocation
     {
-        public override void drawWater(SpriteBatch b)
+        public static void DrawBetweenLayer()
         {
             if (Game1.currentLocation.Map.GetLayer("Between") != null)
                 Game1.currentLocation.Map.GetLayer("Between").Draw(Game1.mapDisplayDevice, Game1.viewport, Location.Origin, false, Game1.pixelZoom);
         }
-        private static void BuildConstructor(TypeBuilder tb, ConstructorInfo parent)
+        private static Type[] GetParamTypes(ParameterInfo[] parameters)
         {
-            ParameterInfo[] parameters = parent.GetParameters();
             Type[] paramTypes = new Type[parameters.Length];
             for (var c = 0; c < parameters.Length; c++)
                 paramTypes[c] = parameters[c].ParameterType;
-            ConstructorBuilder cb = tb.DefineConstructor(parent.Attributes, parent.CallingConvention, paramTypes, null, null);
-            MethodBody constructorBody =parent.GetMethodBody();
-            ILGenerator generator = cb.GetILGenerator();
-            generator.Emit(OpCodes.Ldarg_0);
-            generator.Emit(OpCodes.Ldarg_1);
-            generator.Emit(OpCodes.Ldarg_2);
-            generator.Emit(OpCodes.Call, parent);
-            generator.Emit(OpCodes.Ret);
-        }
-        private static void BuildMethod(TypeBuilder tb, MethodInfo parent, MethodInfo from)
-        {
-            ParameterInfo[] parameters = from.GetParameters();
-            Type[] paramTypes = new Type[parameters.Length];
-            for (var c = 0; c < parameters.Length; c++)
-                paramTypes[c] = parameters[c].ParameterType;
-            MethodBuilder mb = tb.DefineMethod(from.Name, from.Attributes, from.CallingConvention, from.ReturnType, paramTypes);
-            ILGenerator generator = mb.GetILGenerator();
-            generator.Emit(OpCodes.Ldarg_1);
-            generator.Emit(OpCodes.Call, from);
-            generator.Emit(OpCodes.Ldarg_0);
-            generator.Emit(OpCodes.Ldarg_1);
-            generator.Emit(OpCodes.Call, parent);
-            generator.Emit(OpCodes.Ret);
+            return paramTypes;
         }
         public static GameLocation Create(GameLocation location)
         {
-            Type type = typeof(HookedLocation);
             AssemblyName aName = new AssemblyName("Entoarox.HookedLocations");
-            AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly(aName,AssemblyBuilderAccess.RunAndSave);
-            ModuleBuilder mb = ab.DefineDynamicModule(aName.Name, aName.Name + ".dll");
-            TypeBuilder tb = mb.DefineType(location.name + "_Hooked", TypeAttributes.Public,location.GetType());
-            BuildConstructor(tb, location.GetType().GetConstructor(new Type[] { typeof(xTile.Map), typeof(string) }));
-            BuildMethod(tb, location.GetType().GetMethod("drawWater", BindingFlags.Public | BindingFlags.Instance), type.GetMethod("drawWater", BindingFlags.Public | BindingFlags.Instance));
+            AssemblyBuilder assembly = AppDomain.CurrentDomain.DefineDynamicAssembly(aName,AssemblyBuilderAccess.RunAndSave);
+            ModuleBuilder module = assembly.DefineDynamicModule(aName.Name, aName.Name + ".dll");
+            TypeBuilder tb = module.DefineType(location.name + "_Hooked", TypeAttributes.Public,location.GetType());
+            ConstructorInfo cbParent = location.GetType().GetConstructor(new Type[] { typeof(xTile.Map), typeof(string) });
+            ConstructorBuilder cb = tb.DefineConstructor(cbParent.Attributes, cbParent.CallingConvention, GetParamTypes(cbParent.GetParameters()), null, null);
+            MethodBody constructorBody = cbParent.GetMethodBody();
+            ILGenerator cbIL = cb.GetILGenerator();
+            // get [this]
+            cbIL.Emit(OpCodes.Ldarg_0);
+            // get [map]
+            cbIL.Emit(OpCodes.Ldarg_1);
+            // get [name]
+            cbIL.Emit(OpCodes.Ldarg_2);
+            // call [base] as [this] with [map,name]
+            cbIL.Emit(OpCodes.Call, cbParent);
+            // return
+            cbIL.Emit(OpCodes.Ret);
+            MethodInfo mbParent = location.GetType().GetMethod("drawWater", BindingFlags.Public | BindingFlags.Instance);
+            MethodBuilder mb = tb.DefineMethod(mbParent.Name, mbParent.Attributes, mbParent.CallingConvention, mbParent.ReturnType, GetParamTypes(mbParent.GetParameters()));
+            ILGenerator mbIL = mb.GetILGenerator();
+            // call [DrawBetweenLayer]
+            mbIL.Emit(OpCodes.Call, typeof(HookedLocation).GetMethod("DrawBetweenLayer", BindingFlags.Public | BindingFlags.Static));
+            // get [this]
+            mbIL.Emit(OpCodes.Ldarg_0);
+            // get [batch]
+            mbIL.Emit(OpCodes.Ldarg_1);
+            // call [base]
+            mbIL.Emit(OpCodes.Call, mbParent);
+            // return
+            mbIL.Emit(OpCodes.Ret);
             Type result = tb.CreateType();
             return (GameLocation)Activator.CreateInstance(result, new object[] { location.map, location.name });
         }
