ed71accd4bedb75a9a2eb2e0540e80f03f1fe821
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-08-13 21:48:31 -0400

    refactor & document XnbLoader

--- XnbLoader/Mod.cs
+++ XnbLoader/Mod.cs
@@ -1,41 +1,64 @@
 ﻿using System;
 using System.IO;
-
-using StardewModdingAPI;
-
 using Entoarox.Framework;
+using StardewModdingAPI;
 
 namespace Entoarox.XnbLoader
 {
-    using Framework;
     public class XnbLoaderMod : Mod
     {
-        private string _Path;
-        private int Files = 0;
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
             Helper.RequestUpdateCheck("https://raw.githubusercontent.com/Entoarox/StardewMods/master/XnbLoader/update.json");
-            _Path = Path.Combine(Helper.DirectoryPath, "ModContent", "");
-            Directory.CreateDirectory(_Path);
-            Monitor.Log("Parsing `ModContent` for files to redirect the content manager to...", LogLevel.Info);
-            ParseDir(_Path);
-            Monitor.Log("Reloading static content references...", LogLevel.Trace);
-            Monitor.Log($"Parsing complete, found and redirected [{Files}] files", LogLevel.Info);
+
+            // get content path
+            string path = Path.Combine(Helper.DirectoryPath, "ModContent");
+            Directory.CreateDirectory(path);
+
+            // load files
+            this.Monitor.Log("Parsing `ModContent` for files to redirect the content manager to...", LogLevel.Info);
+            int overrides = this.LoadOverrides(path, path);
+            this.Monitor.Log($"Parsing complete, found and redirected {overrides} files", LogLevel.Info);
         }
-        private void ParseDir(string path)
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Recursively find XNBs and register them with the content registry.</summary>
+        /// <param name="root">The root path being searched.</param>
+        /// <param name="path">The path for which to load XNBs.</param>
+        private int LoadOverrides(string root, string path)
         {
-            string separator = Path.DirectorySeparatorChar.ToString();
-            Monitor.Log("Scanning for files and directories in " + path.Replace(_Path + separator, separator).Replace(_Path, separator), LogLevel.Trace);
+            int files = 0;
+
+            // log path
+            {
+                string pathSeparator = Path.DirectorySeparatorChar.ToString();
+                string relativePath = path.Replace(root + pathSeparator, pathSeparator).Replace(root, pathSeparator);
+                this.Monitor.Log($"Scanning for files and directories in {relativePath}", LogLevel.Trace);
+            }
+
+            // load subfolders
             foreach (string dir in Directory.EnumerateDirectories(path))
-                ParseDir(Path.Combine(path, dir));
+                files += this.LoadOverrides(root, Path.Combine(path, dir));
+
+            // load files
             foreach (string file in Directory.EnumerateFiles(path))
             {
                 string filePath = Path.Combine(path, Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file));
-                string from = filePath.Replace(_Path+separator, "");
-                Monitor.Log($"Redirecting: {from} ~> {filePath}.xnb", LogLevel.Trace);
-                Files++;
+                string from = filePath.Replace(root + Path.DirectorySeparatorChar, "");
+                this.Monitor.Log($"Redirecting: {from} ~> {filePath}.xnb", LogLevel.Trace);
                 Helper.Content.RegisterXnbReplacement(from, filePath);
+                files++;
             }
+
+            return files;
         }
     }
 }
