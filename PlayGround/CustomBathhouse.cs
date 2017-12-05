1e3c055d643947abadeb665744b797a446e664cf
Entoarox <kvdk@kvdk.net>
2017-12-05 22:16:55 +0100

    PG ~ My shame

--- PlayGround/CustomBathhouse.cs
+++ PlayGround/CustomBathhouse.cs
@@ -34,17 +34,51 @@ namespace PlayGround
             base.drawAboveAlwaysFrontLayer(b);
             // End vanilla
             b.End();
+            Rectangle viewport = new Rectangle(0, 0, Game1.viewport.Width, Game1.viewport.Height);
             // Custom clamping
-            b.GraphicsDevice.ScissorRectangle = Rectangle.Intersect(new Rectangle(15 * Game1.tileSize - Game1.viewport.X, 1 * Game1.tileSize - Game1.viewport.Y, 25 * Game1.tileSize, 20 * Game1.tileSize - 7*Game1.pixelZoom), new Rectangle(0, 0, Game1.viewport.Width, Game1.viewport.Height));
+            b.GraphicsDevice.ScissorRectangle = Rectangle.Intersect(new Rectangle(15 * Game1.tileSize - Game1.viewport.X, 1 * Game1.tileSize - Game1.viewport.Y, 25 * Game1.tileSize, 20 * Game1.tileSize - 7*Game1.pixelZoom), viewport);
             b.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, null, new RasterizerState() { ScissorTestEnable = true });
-            for (float num = this.SteamPosition.X; num < Game1.graphics.GraphicsDevice.Viewport.Width+Game1.viewport.X + 256f; num += 256f)
+            for (float num = this.SteamPosition.X; num < Game1.viewport.Width+Game1.viewport.X + 256f; num += 256f)
             {
-                for (float num2 = this.SteamPosition.Y; num2 < Game1.graphics.GraphicsDevice.Viewport.Height+Game1.viewport.Y + 128; num2 += 256f)
+                for (float num2 = this.SteamPosition.Y; num2 < Game1.viewport.Height+Game1.viewport.Y + 128; num2 += 256f)
                 {
-                    b.Draw(this.SteamAnimation, new Rectangle((int)Math.Round(num - Game1.viewport.X), (int)Math.Round(num2 - Game1.viewport.Y), 256, 256), null, Color.White * 0.8f, 0f, Vector2.Zero, SpriteEffects.None, 1f);
+                    b.Draw(this.SteamAnimation, new Rectangle((int)Math.Round(num - Game1.viewport.X), (int)Math.Round(num2 - Game1.viewport.Y), 256, 256), null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 1f);
                 }
             }
             b.End();
+            // Left-door magic
+            float px = Game1.tileSize * 2 + 1;
+            double pxp = 1 / px;
+            int h = 4 * Game1.tileSize - 7 * Game1.pixelZoom;
+            int y = 9 * Game1.tileSize - Game1.viewport.Y;
+            int x = 15 * Game1.tileSize - Game1.viewport.X;
+            for (int c = 1; c < px; c++)
+            {
+                b.GraphicsDevice.ScissorRectangle = Rectangle.Intersect(new Rectangle(x - c, y, 1, h), viewport);
+                b.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, null, new RasterizerState() { ScissorTestEnable = true });
+                for (float num = this.SteamPosition.X; num < Game1.viewport.Width + Game1.viewport.X + 256f; num += 256f)
+                {
+                    for (float num2 = this.SteamPosition.Y; num2 < Game1.viewport.Height + Game1.viewport.Y + 128; num2 += 256f)
+                    {
+                        b.Draw(this.SteamAnimation, new Rectangle((int)Math.Round(num - Game1.viewport.X), (int)Math.Round(num2 - Game1.viewport.Y), 256, 256), null, Color.White * (float)(pxp*(px-c)), 0f, Vector2.Zero, SpriteEffects.None, 1f);
+                    }
+                }
+                b.End();
+            }
+            x += (25 * Game1.tileSize) - 1;
+            for (int c = 1; c < px; c++)
+            {
+                b.GraphicsDevice.ScissorRectangle = Rectangle.Intersect(new Rectangle(x + c, y, 1, h), viewport);
+                b.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, null, new RasterizerState() { ScissorTestEnable = true });
+                for (float num = this.SteamPosition.X; num < Game1.viewport.Width + Game1.viewport.X + 256f; num += 256f)
+                {
+                    for (float num2 = this.SteamPosition.Y; num2 < Game1.viewport.Height + Game1.viewport.Y + 128; num2 += 256f)
+                    {
+                        b.Draw(this.SteamAnimation, new Rectangle((int)Math.Round(num - Game1.viewport.X), (int)Math.Round(num2 - Game1.viewport.Y), 256, 256), null, Color.White * (float)(pxp * (px - c)), 0f, Vector2.Zero, SpriteEffects.None, 1f);
+                    }
+                }
+                b.End();
+            }
             // Restore vanilla
             Game1.spriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend, SamplerState.PointClamp, null, null);
         }
