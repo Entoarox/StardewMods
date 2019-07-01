da679f9df4d0e9ae02b6e4e7c92216a6cd7b70b2
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-07-01 14:26:26 -0400

    migrate to new .csproj format

--- FurnitureAnywhere/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("FurnitureAnywhere")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("1.1.7")]
-[assembly: AssemblyFileVersion("1.1.7")]
--- ShopExpander/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("ShopExpander")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("1.6.2")]
-[assembly: AssemblyFileVersion("1.6.2")]
